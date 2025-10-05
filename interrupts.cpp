7e9a24297f7b33f854f291b9a90a03f839cc4c59
Ajan <ajanbalaganesh@cmail.carleton.ca>
2025-10-05 03:02:32 -0400

    Code for SYSC4001-A1

--- interrupts.cpp
+++ interrupts.cpp
@@ -2,10 +2,11 @@
  *
  * @file interrupts.cpp
  * @author Sasisekhar Govind
- *
+ * @author Ajan Balaganesh
+ * @author Kyle Deng
  */
 
-#include<interrupts.hpp>
+#include"interrupts.hpp"
 
 int main(int argc, char** argv) {
 
@@ -19,7 +20,10 @@ int main(int argc, char** argv) {
     std::string execution;  //!< string to accumulate the execution output
 
     /******************ADD YOUR VARIABLES HERE*************************/
-
+    int now = 0;
+    int context_save_ms = 10;
+    // int context_save_ms = 40;
+    
 
 
     /******************************************************************/
@@ -30,6 +34,66 @@ int main(int argc, char** argv) {
 
         /******************ADD YOUR SIMULATION CODE HERE*************************/
 
+	if (activity == "CPU") {
+            int cpu_ms = duration_intr;
+            
+            execution += std::to_string(now) + ", " + std::to_string(cpu_ms) + ", CPU burst\n";
+            now += cpu_ms;
+        }
+        else if (activity == "SYSCALL" || activity == "END_IO") {
+            int dev = duration_intr;
+
+
+
+            
+            auto [prelude, updated_time] = intr_boilerplate(now, dev, context_save_ms, vectors);
+            execution += prelude;
+            now = updated_time;
+
+            int isr_body_ms = delays.at(dev);
+	    //int isr_body_ms = 100;
+            //int isr_body_ms = 400;
+            if (activity == "SYSCALL") {
+		int remaining = isr_body_ms;
+                execution += std::to_string(now) + ", " + std::to_string(40)
+                           + ", Sys Call(Run the ISR)\n";
+		now += 40;
+		remaining -= 40;
+		if (remaining < 41){
+                	execution += std::to_string(now) + ", " + std::to_string(remaining - 1)
+                           	+ ", transfer data from device to memory\n";
+			now += (remaining - 1);
+			remaining = 1;
+                	execution += std::to_string(now) + ", " + std::to_string(1)
+                           + ", check for errors\n";
+			now += 1;
+			remaining = 0;
+                }else {
+                	execution += std::to_string(now) + ", " + std::to_string(40)
+                           + ", transfer data from device to memory\n";
+			now += 40;
+			remaining -= 40;
+                	execution += std::to_string(now) + ", " + std::to_string(remaining)
+                           + ", check for errors\n";
+			now += remaining;
+			remaining = 0;
+		}
+                
+	
+            } else {
+                execution += std::to_string(now) + ", " + std::to_string(40)
+                           + ", ENDIO(Run the ISR)\n";
+		now += 40;
+	        execution += std::to_string(now) + ", " + std::to_string(isr_body_ms - 40)
+                           + ", Check device status\n";	
+		now += (isr_body_ms - 40);
+            }
+            //now += isr_body_ms;
+
+            
+            execution += std::to_string(now) + ", " + std::to_string(1) + ", IRET\n";
+            now += 1;
+        }
 
 
         /************************************************************************/
